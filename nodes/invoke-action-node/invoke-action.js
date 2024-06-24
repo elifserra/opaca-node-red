ce7b4d37823e0bea45333fa16e35b28d125410b7
elifserra <123359468+elifserra@users.noreply.github.com>
2024-06-24 16:13:48 +0300

    Update invoke-action.js

--- nodes/invoke-action-node/invoke-action.js
+++ nodes/invoke-action-node/invoke-action.js
@@ -5,7 +5,6 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', async function(msg) {
-            //const x = fetchData(node);
             var choices = config.choices;
             msg.payload =  choices;
 
