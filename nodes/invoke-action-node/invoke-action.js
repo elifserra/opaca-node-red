5ec401bc1f30424f0f7165b8daa63eff13793a67
oruccakir <oruccakir2525@gmail.com>
2024-07-01 12:09:35 +0200

    comments and documentation added to invoke actions

--- nodes/invoke-action-node/invoke-action.js
+++ nodes/invoke-action-node/invoke-action.js
@@ -1,4 +1,10 @@
-async function invokeAction(endpoint, queryString, msg){
+/**
+ * Function to invoke an action on the server.
+ * @param {string} endpoint - The endpoint to invoke the action.
+ * @param {string} queryString - The query string to send in the request body.
+ * @param {object} msg - The message object to update with the response.
+ */
+async function invokeAction(endpoint, queryString, msg) {
     var url = "http://localhost:8000/invoke/" + endpoint;
 
     try {
@@ -8,63 +14,65 @@ async function invokeAction(endpoint, queryString, msg){
                 'Content-Type': 'application/json'  // Inform the server that the body is JSON
             },
             body: queryString
-        })
+        });
         
-        await response.json().then( data => {
-            msg.payload = data;
-        }
-            
-        );
-       
+        await response.json().then(data => {
+            msg.payload = data; // Update the message payload with the response data
+        });
     } catch (error) {
-        console.error("Fetch error: " + error);
+        console.error("Fetch error: " + error); // Log any errors during the fetch
     }
 }
 
-
-
+/**
+ * Convert an array of parameters to a JSON string.
+ * @param {Array} parameterArray - The array of parameters to convert.
+ * @returns {string} - The JSON string representation of the parameters.
+ */
 function toJsonString(parameterArray) {
-
-    //console.log("inside the function");
     var jsonString = "{";
-    var length = parameterArray.length-1;
+    var length = parameterArray.length - 1;
     var count = 0;
 
-
     parameterArray.forEach(element => {
-        jsonString += "\"" + element.value.name + "\":";
-
-        element.value.type === "string" ? jsonString += "\"" + element.value.value + "\"" : jsonString += element.value.value;
-        count !== length ? jsonString += "," : jsonString += "}";
-
+        jsonString += "\"" + element.value.name + "\":"; // Add parameter name
+        element.value.type === "string" ? jsonString += "\"" + element.value.value + "\"" : jsonString += element.value.value; // Add parameter value
+        count !== length ? jsonString += "," : jsonString += "}"; // Add comma between parameters except the last one
         count++;
     });
-    console.log(jsonString);
-   
-    return jsonString;
 
-
+    console.log(jsonString); // Log the JSON string for debugging
+    return jsonString; // Return the JSON string
 }
 
+/**
+ * Module export function to define the custom Node-RED node.
+ * @param {object} RED - The Node-RED module object.
+ */
 module.exports = function(RED) {
-
+    /**
+     * Constructor function for the custom node.
+     * @param {object} config - The node configuration object.
+     */
     function MyNode(config) {
-        RED.nodes.createNode(this, config);
+        RED.nodes.createNode(this, config); // Create the node with the given configuration
 
-        this.paramOutputs = config.paramOutputs;
-        this.parameters = config.parameters;
-        this.action = config.action;
+        this.paramOutputs = config.paramOutputs; // Parameter outputs from configuration
+        this.parameters = config.parameters; // Parameters from configuration
+        this.action = config.action; // Action from configuration
 
         var node = this;
 
+        /**
+         * Event handler for node input.
+         * @param {object} msg - The input message object.
+         */
         node.on('input', async function(msg) {
-            
-            await invokeAction(node.action, toJsonString(node.paramOutputs), msg);
-
-            node.send(msg);
-            
+            await invokeAction(node.action, toJsonString(node.paramOutputs), msg); // Invoke the action with parameters
+            node.send(msg); // Send the updated message object
         });
     }
-    
+
+    // Register the custom node type with Node-RED
     RED.nodes.registerType("invoke-action", MyNode);
-}
+};
