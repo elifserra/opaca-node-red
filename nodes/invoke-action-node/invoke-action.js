9bb2576f5add59751a3fa75780e724503d9a169e
oruccakir <oruccakir2525@gmail.com>
2024-07-01 11:01:59 +0200

    save paramters button deleted and blue light implemented

--- nodes/invoke-action-node/invoke-action.js
+++ nodes/invoke-action-node/invoke-action.js
@@ -1,120 +1,70 @@
-// Description: This file contains the code for the node that invokes the action on the device.
+async function invokeAction(endpoint, queryString, msg){
+    var url = "http://localhost:8000/invoke/" + endpoint;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'  // Inform the server that the body is JSON
+            },
+            body: queryString
+        })
+        
+        await response.json().then( data => {
+            msg.payload = data;
+        }
+            
+        );
+       
+    } catch (error) {
+        console.error("Fetch error: " + error);
+    }
+}
+
+
+
+function toJsonString(parameterArray) {
+
+    //console.log("inside the function");
+    var jsonString = "{";
+    var length = parameterArray.length-1;
+    var count = 0;
+
+
+    parameterArray.forEach(element => {
+        jsonString += "\"" + element.value.name + "\":";
+
+        element.value.type === "string" ? jsonString += "\"" + element.value.value + "\"" : jsonString += element.value.value;
+        count !== length ? jsonString += "," : jsonString += "}";
+
+        count++;
+    });
+    console.log(jsonString);
+   
+    return jsonString;
+
+
+}
+
 module.exports = function(RED) {
 
     function MyNode(config) {
-        RED.nodes.createNode(this, config);                                // creating the node
-        var node = this;                                                   // creating a reference to the node
+        RED.nodes.createNode(this, config);
 
-        // Retrieve the parameters from the configuration node 
-        node.name = config.name;                                          // name of the node
-        node.action = config.action;                                      // selected action to be invoked
-        node.actionsList = config.actionsList;                            // list of actions
-        node.actionParams = config.actionParams;                          // map arameters of the action
-        node.parameters = config.parameters;                              // list of parameters
+        this.paramOutputs = config.paramOutputs;
+        this.parameters = config.parameters;
+        this.action = config.action;
 
-        node.on('input', function(msg) {   
-            node.warn(config.action);   
-            node.warn(config.actionsList);
-            node.warn(config.parameters);    
-            msg.payload = node.parameters;
-            node.send(msg);                                               // sending the message to the next node 
+        var node = this;
+
+        node.on('input', async function(msg) {
+            
+            await invokeAction(node.action, toJsonString(node.paramOutputs), msg);
+
+            node.send(msg);
+            
         });
-
     }
     
-   RED.nodes.registerType("invoke-action", MyNode);                       // registering the node
-
+    RED.nodes.registerType("invoke-action", MyNode);
 }
-
-/*
-oneditprepare: async function() {
-
-    var that = this;
-    
-    if(that.parameters){
-        that.paramsHtml = "";
-        for (var key in that.parameters) {
-            that.paramsHtml += `<label for="${key}">${key}</label><input type="text" id="${key}" value="${that.parameters[key]}" placeholder="${that.parameters[key]}">`;
-        }
-    }
-    
-       
-    $("#save-parameters-button").on('click', function () {
-
-        that.nodeParametersBoxes.forEach(box => {
-            var inputElement = $(`#${box.id}`);
-            box.value = inputElement.val(); // Get the value of each input element
-            that.parameters[box.id] = box.value;
-        });
-
-        console.log("Save parameters button clicked");
-        //console.log(this.parameters);
-
-    }.bind(this));
-
-    async function fetchData() {
-        const apiUrl = "http://localhost:8000/agents";
-        var actList = [];    
-        try {
-            const response = await fetch(apiUrl, { method: 'GET' });
-            const actions = [];
-            const data = await response.json();
-            data.forEach(agent => {
-                if (agent.actions) {
-                    agent.actions.forEach(action => {
-                        actions.push(action);
-                    });
-                }
-            });
-            return actions;
-        } catch (error) {
-            console.error("Fetch error:", error);   
-            return null;    
-        }
-    }
-
-
-    var actions = await fetchData();                                                     // Fetch actions from API
-    that.actionsList = actions.map(action => action.name);                               // Actions list
-    that.actionParams = actions.map(action => [action.name,action.parameters]);          // Action parameters as key-value pairs key: {name: param}
-
-
-    $("#node-input-action").empty().append(`<option value="${that.action}">${that.action}</option>`);
-    that.actionsList.forEach(action => {
-        $("#node-input-action").append(`<option value="${that.action}">${that.action}</option>`);
-    });
-
-    $("#parameters-container").empty().append(that.paramsHtml);
-
-    $("#node-input-action").on('change', function() {
-        that.action = $("#node-input-action").val();
-        var selectedActionParams = that.actionParams.find(action => action[0] === that.action);
-        var params = selectedActionParams[1];
-        that.paramsHtml = "";
-        that.parameters = [];
-        that.nodeParametersBoxes = [];
-        for (var key in params) {
-            that.paramsHtml += `<label for="${key}">${key}</label><input type="text" id="${key}" placeholder="${params[key].type}">`;
-            that.nodeParametersBoxes.push({ id: `${key}`, value: null });                     // Store the input boxes to access them later
-        }
-        $("#parameters-container").empty().append(that.paramsHtml);
-    }.bind(this));
-
-
-
-},
-
-
-
-oneditsave: function() { 
-    
-    var that = this;
-
-    that.nodeParametersBoxes.forEach(box => {
-        var inputElement = $(`#${box.id}`);
-        box.value = inputElement.val(); // Get the value of each input element
-        that.parameters[box.id] = box.value;
-    });
-    
-}
-*/
