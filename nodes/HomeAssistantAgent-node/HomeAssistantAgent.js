20380ad0c559f5777df7579463b594c55e8e7b24
oruccakir <oruccakir2525@gmail.com>
2024-07-08 09:24:47 +0200

    unnecessary agents deleted, instead configurable and parameterized agent created and deployed

--- nodes/HomeAssistantAgent-node/HomeAssistantAgent.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(RED) {
-
-    function HomeAssistantNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-
-    }
-    RED.nodes.registerType("HomeAssistantAgent", HomeAssistantNode);
-}
