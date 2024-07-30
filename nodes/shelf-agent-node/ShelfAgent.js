23cdc176b7cffc1fb50b05c374ca77e814bdba18
oruccakir <oruccakir2525@gmail.com>
2024-07-30 11:27:36 +0200

    shelf agent node arranged, documentation will be added

--- nodes/shelf-agent-node/ShelfAgent.js
+++ nodes/shelf-agent-node/ShelfAgent.js
@@ -5,26 +5,13 @@ module.exports = function(RED){
     function ShelfAgentNode(config){
         RED.nodes.createNode(this,config);  
         var node = this;
-        this.paramOutputs = config.paramOutputs;
-        this.parameters = config.parameters;
-        this.action = config.action;
-        this.agentId = config.agentId;
-        this.actions = config.actions;
-
-        var actions = null;
-        try{
-            actions = node.context().global.get(this.agentId);
-        }
-        catch(error){
-            
-        }
-    
+        node.paramOutputs = config.paramOutputs;
+        node.action = config.action;
 
         node.on('input', async function(msg){   
             console.log(helper_methods.toJsonString(node.paramOutputs,msg));
             await helper_methods.invokeAction(node.action, helper_methods.toJsonString(node.paramOutputs,msg), msg,node); // Invoke the action with parameters
             node.send(msg); // Send the message to the next node
-            node.warn(actions); // Log the actions for debugging
         });
 
     }
