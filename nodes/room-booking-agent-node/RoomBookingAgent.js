b5279f5b5cbfbe714b3ca02a27680cad56680186
oruccakir <oruccakir2525@gmail.com>
2024-07-30 11:25:15 +0200

    room booking agent node arranged, documentation will be added

--- nodes/room-booking-agent-node/RoomBookingAgent.js
+++ nodes/room-booking-agent-node/RoomBookingAgent.js
@@ -5,25 +5,12 @@ module.exports = function(RED){
     function RoomBookingAgentNode(config){
         RED.nodes.createNode(this,config);  
         var node = this;
-        this.paramOutputs = config.paramOutputs;
-        this.parameters = config.parameters;
-        this.action = config.action;
-        this.agentId = config.agentId;
-        this.actions = config.actions;
-
-        var actions = null;
-        try{
-            actions = node.context().global.get(this.agentId);
-        }
-        catch(error){
-            
-        }
-    
+        node.paramOutputs = config.paramOutputs;
+        node.action = config.action;
 
         node.on('input', async function(msg){   
             await helper_methods.invokeAction(node.action, helper_methods.toJsonString(node.paramOutputs,msg), msg,node); // Invoke the action with parameters
             node.send(msg); // Send the message to the next node
-            node.warn(actions); // Log the actions for debugging
         });
 
     }
