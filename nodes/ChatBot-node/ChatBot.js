469bc3fb345dcfc5aa8465a503c9fc0cbe05c408
oruccakir <oruccakir2525@gmail.com>
2024-07-30 11:38:13 +0200

    unnecessary lines of code deleted from chatBot.js

--- nodes/ChatBot-node/ChatBot.js
+++ nodes/ChatBot-node/ChatBot.js
@@ -3,24 +3,8 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         var openaiAPIKey = process.env.OPENAI_API_KEY;
-        var loginUrl = process.env.LOGIN_URL;
-        console.log("Login URL: " + loginUrl);
-        console.log("OpenAI API Key: " + openaiAPIKey);
         RED.httpAdmin.get('/openAIKey', function(req, res) {
             res.json({ value: openaiAPIKey });
-        });
-
-
-        // Event listener kurma
-        RED.events.on('input', function(event) {
-            node.warn("ChatBot node received message: " + JSON.stringify(event.payload));
-            if (event.id === node.id) {
-                node.warn("ChatBot node received message: " + JSON.stringify(event.payload));
-                
-                // Alınan mesajı işleme ve başka bir node'a gönderme
-                var msg = { payload: event.payload };
-                node.send(msg);
-            }
         });
 
         node.on('input', function(msg) {
