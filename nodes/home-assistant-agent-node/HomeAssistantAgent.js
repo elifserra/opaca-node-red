90c9942805566f2dea5ae5069040f6642c38999c
oruccakir <oruccakir2525@gmail.com>
2024-07-30 11:22:14 +0200

    home assistamt node arranged, documentation will be added

--- nodes/home-assistant-agent-node/HomeAssistantAgent.js
+++ nodes/home-assistant-agent-node/HomeAssistantAgent.js
@@ -5,25 +5,13 @@ module.exports = function(RED){
     function HomeAssistantAgentNode(config){
         RED.nodes.createNode(this,config);  
         var node = this;
-        this.paramOutputs = config.paramOutputs;
-        this.parameters = config.parameters;
-        this.action = config.action;
-        this.agentId = config.agentId;
-        this.actions = config.actions;
+        node.paramOutputs = config.paramOutputs;
+        node.action = config.action;
 
-        var actions = null;
-        try{
-            actions = node.context().global.get(this.agentId);
-        }
-        catch(error){
-            
-        }
-         
         node.on('input', async function(msg){   
             node.warn(helper_methods.toJsonString(node.paramOutputs,msg));
             await helper_methods.invokeAction(node.action, helper_methods.toJsonString(node.paramOutputs,msg), msg,node); // Invoke the action with parameters
             node.send(msg); // Send the message to the next node
-            node.warn(actions); // Log the actions for debugging
         });
 
     }
