4750cdb99cca2ba949cfc8acf18d9698f3586fb7
oruccakir <oruccakir2525@gmail.com>
2024-08-05 09:03:22 +0200

    All class logic implemented to all agents

--- nodes/resources/html_common_methods.js
+++ nodes/resources/html_common_methods.js
@@ -9,12 +9,13 @@ class Agent{
         this.agentNumberOfInputs = agentNumberOfInputs;
         this.agentNumberOfOutputs = agentNumberOfOutputs;
         this.currentAction = null;
+        this.actions = null;
     }
 
     async fetchAgentActions() {
+        this.actions = [];
         var fetchedAgent = await fetch(`${this.agentID}`).then(response => response.json());
         var fetchedActions = fetchedAgent.value;
-        this.actions = [];
         fetchedActions.forEach(action => {
             this.actions.push(new Action(action.name,action.parameters));
         });
@@ -37,10 +38,72 @@ class Agent{
                 default: this.currentAction.actionParameters[key].typedInputType,
                 typefield: $(`#${this.currentAction.actionParameters[key].name}-type`)
             });
+        }   
+    }
+
+    appenTheSelectedAgentCommonHtml(){
+
+        fetch('http://127.0.0.1:1880/common_html_template.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            $("#dialog-form").empty().append(data);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+        
+    }
+
+
+    async oneditPrepareFunction(){
+
+        var agent = this;
+        console.log(this);
+
+        console.log("oneditPrepareFunction");
+
+        this.appenTheSelectedAgentCommonHtml();
+
+        await this.fetchAgentActions();
+
+        if(this.currentAction != null){
+            this.currentAction.saveParametersHtml();
         }
-            
+
+        $("#node-input-action").empty().append(`<option value=${""}>${this.currentAction === null ? "" : this.currentAction.actionName}</option>`);
+        this.actions.forEach(action => {
+            $("#node-input-action").append(`<option value="${action.actionName}">${action.actionName}</option>`);
+        });
+
+        $("#node-input-action").on('change', function(){
+            this.applyChangesForActionChange();
+        }.bind(this)); 
+
+        $("#invoke-action-button").on('click', async function(){
+            await agent.currentAction.handleInvokeAction();
+        });
 
     }
+
+    oneditSaveFunction(){
+        console.log("oneditSaveFunction");
+        if(this.currentAction != null){
+            this.currentAction.saveParameters();
+        }
+    }
+
+    oneditCancelFunction(){
+        console.log("oneditCancelFunction");
+        if(this.currentAction != null){
+            this.currentAction.saveParameters();
+        }
+    }
+
 }
 
 
@@ -101,10 +164,6 @@ class Action{
 
     }
 
-    display(){
-        console.log("çekno");
-    }
-
     toJsonString() {
         var actualValue;
         var valueAsPassed;
@@ -150,19 +209,15 @@ class Action{
 
 
     async handleInvokeAction(){
+        console.log("handleInvokeAction");
         this.saveParameters();
         var query_string = this.toJsonString();
+        console.log(query_string);
         var result = await this.invokeAction(query_string); // Invoke the action with parameters
-        const resultContainer = document.getElementById('result-container');
-        const resultText = document.getElementById('result-text');
-        resultText.textContent = result;
-        resultContainer.classList.remove('hidden');
+        console.log(result);
+        $("#result-text").text(result);
+        $("#result-container").removeClass("hidden");
     }
-
-
-
-
-    
 
 }
 
@@ -173,23 +228,6 @@ class Parameter {
         this.value = value;
         this.typedInputType = "str";
     }
-}
-
-
-common_defaults = {
-    name: { value: "" },
-    agentId: { value: "" },
-    actions: { value: null },
-    actionsList: { value: [] },
-    actionParams: { value: [] },
-    action: { value: "" },
-    paramsHtml: { value: "" },
-    paramOutputs: { value: [] },
-    nodeParametersBoxes: { value: [] },
-    parameters: { value: [] },
-    defaultTypes: { value: [] },
-    isAgentIDChanged: { value: false },
-    agent:{value:null}
 }
 
 
@@ -205,32 +243,6 @@ async function getParametersOfSelectedAgent(that){
 }
 
 
-async function getThisAgentNodeActions(that){
-
-    var data = await fetch(`${that.agentId}`).then(response => response.json());
-    that.actions = data.value;
-    that.actionsList = that.actions.map(action => action.name);
-    that.actionParams = that.actions.map(action => [action.name, action.parameters]);
-}
-
-function updateParametersHtmlSection(that){
-    if (that.parameters) {
-        that.paramsHtml = "";
-        for (var key in that.parameters) {
-            that.paramsHtml += `<label for="${key}">${key}</label><input type="text" id="${key}" value="${that.parameters[key]}" placeholder="${that.parameters[key]}">`;
-        }
-    }
-    $("#parameters-container").empty().append(that.paramsHtml);
-
-    for (var key in that.parameters) {
-        $(`#${key}`).typedInput({
-            types: ['str', 'msg'],
-            default: that.defaultTypes[key],
-            typefield: $(`#${key}-type`)
-        });
-    }
-}
-
 async function applyChangesForAgentChange(that){
     that.agentId = $("#node-input-agentId").val();
     $("#node-input-name").val(that.agentId);
@@ -244,166 +256,63 @@ async function applyChangesForAgentChange(that){
     $("#node-input-action").empty();
 }
 
-function applyChangesForActionChange(that){
-    that.action = $("#node-input-action").val();
-    $("#node-input-action").val(that.action);
-    var selectedActionParams = that.actionParams.find(action => action[0] === that.action);
-    var params = selectedActionParams[1];
-    that.paramsHtml = "";
-    that.nodeParametersBoxes = [];
-    that.paramOutputs = [];
-    for (var key in params) {
-        that.paramsHtml += `<label for="${key}">${key}</label>
-                            <input type="text" id="${key}" placeholder="${params[key].type}">`;
-        that.nodeParametersBoxes.push({ id: `${key}`, value: null });
-        that.paramOutputs.push({ id: `${key}`, value: new Parameter(key, params[key].type, null) });
-    }
-    $("#parameters-container").empty().append(that.paramsHtml);
 
-    for (var key in params) {
-        $(`#${key}`).typedInput({
-            types: ['str', 'msg'],
-            default: 'str',
-            typefield: $(`#${key}-type`)
-        });
-    }
-}
+const shelfAgentName = "ShelfAgent";
+const shelfAgentLabel = "Shelf Agent";
+const shelfAgentID = "shelf-agent";
+const shelfAgentColor = "red";
+const shelfAgentIcon = "shelf-agent";
+const shelfAgentCategory = "ZEKI";
+const shelfAgentNumberOfInputs = 1;
+const shelfAgentNumberOfOutputs = 1;
+
+const fridgeAgentName = "FridgeAgent";
+const fridgeAgentLabel = "Fridge Agent";
+const fridgeAgentID = "fridge-agent";
+const fridgeAgentColor = "gray";
+const fridgeAgentIcon = "fridge-agent";
+const fridgeAgentCategory = "ZEKI";
+const fridgeAgentNumberOfInputs = 1;
+const fridgeAgentNumberOfOutputs = 1;
+
+const wayFindingAgentName = "WayFindingAgent";
+const wayFindingAgentLabel = "Way Finding Agent";
+const wayFindingAgentID = "way-finding-agent";
+const wayFindingAgentColor = "green";
+const wayFindingAgentIcon = "way-finding-agent";
+const wayFindingAgentCategory = "ZEKI";
+const wayFindingAgentNumberOfInputs = 1;
+
+const sensorAgentName = "SensorAgent";
+const sensorAgentLabel = "Sensor Agent";
+const sensorAgentID = "servlet-agent";
+const sensorAgentColor = "brown";
+const sensorAgentIcon = "servlet-agent";
+const sensorAgentCategory = "ZEKI";
+const sensorAgentNumberOfInputs = 1;
+const sensorAgentNumberOfOutputs = 1;
+
+const homeAssistantAgentName = "HomeAssistantAgent";
+const homeAssistantAgentLabel = "Home Assistant Agent";
+const homAssistantAgentID = "home-assistant-agent";
+const homeAssistantAgentColor = "orange";
+const homeAssistantAgentIcon = "home-assistant-agent";
+const homeAssistantAgentCategory = "ZEKI";
+const homeAssistantAgentNumberOfInputs = 1;
+const homeAssistantAgentNumberOfOutputs = 1;
+
+const roomBookingAgentName = "RoomBookingAgent";
+const roomBookingAgentLabel = "Room Booking Agent";
+const roomBookingAgentID = "room-booking-agent";
+const roomBookingAgentColor = "white";
+const roomBookingAgentIcon = "room-booking-agent";
+const roomBookingAgentCategory = "ZEKI";
+const roomBookingAgentNumberOfInputs = 1;
+const roomBookingAgentNumberOfOutputs = 1;
 
 
 
-async function saveParameters(node) {
-    var types = {};
-    node.nodeParametersBoxes.forEach(box => {
-        var inputElement = $(`#${box.id}`);
-        box.value = inputElement.val(); 
-        types[box.id] = inputElement.typedInput('type');
-    });
 
-    node.defaultTypes = types;
 
-    var parameters = {};
-    node.nodeParametersBoxes.forEach(box => {
-        parameters[box.id] = box.value; 
-        node.paramOutputs.forEach(param => {
-            if (param.id === box.id) {
-                param.value.value = box.value; 
-            }
-        });
-    });
 
-    node.parameters = parameters;
 
-    RED.nodes.dirty(true); 
-
-}
-
-function appendTheCommonHTMLFile(){
-    fetch('http://127.0.0.1:1880/common_html_template.html')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.text();
-    })
-    .then(data => {
-        document.querySelector('#dialog-form').innerHTML += data;
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });
-}
-
-async function invokeActionForHTMLSide(endpoint, queryString,node) {
-
-    var data = await fetch('token').then(response=>response.json());
-    const token = data.value;
-    // Construct the URL for the API call
-    var url = "http://10.42.6.107:8000/invoke/" + endpoint;
-    try {
-        // Make a POST request to the specified URL with the query string
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',  
-                Authorization: `Bearer ${token}`
-            },
-            body: queryString
-        });
-
-        var result = response.json();
-        return result;
-        //${node.context().global.get("token")}
-        // Parse the JSON response and set the payload of the message
-
-    } catch (error) {
-        // Log any errors that occur during the API call
-        node.error("INVOKE ACTION ERROR : " + error);    // In case of any error, this is displayed on the debug screen of node-red website
-        return null;
-    }
-}
-
-function toJsonStringForHTMLSide(parameterArray) {
-    var actualValue;
-    var valueAsPassed;
-    var jsonString = "{";
-    var count = 0;
-    var length = parameterArray.length - 1;
-
-    // Loop through each element in the parameter array
-    parameterArray.forEach(element => {
-        // Add the parameter name to the JSON string
-        jsonString += "\"" + element.value.name + "\":"; 
-        actualValue = element.value.value;
-
-        // Determine the actual value based on the parameter type and value
-        element.value.type === "string" ? valueAsPassed = `"${actualValue}"` : valueAsPassed = actualValue;
-
-        // Add the value to the JSON string
-        jsonString += valueAsPassed;
-
-        // Add a comma if this is not the last element
-        count !== length ? jsonString += "," : jsonString += "}";
-        count++;
-    });
-
-    return jsonString;
-}
-
-async function handleInvokeAction(that){
-    document.getElementById("invoke-action-button").addEventListener('click', async function(){
-        saveParameters(that); // this is crucial
-        var query_string = toJsonStringForHTMLSide(that.paramOutputs);
-        var result = await invokeActionForHTMLSide(that.action,query_string,that); // Invoke the action with parameters
-        // Show the result in the result container
-        const resultContainer = document.getElementById('result-container');
-        const resultText = document.getElementById('result-text');
-        resultText.textContent = result;
-        resultContainer.classList.remove('hidden');
-    })
-}
-
-async function commonOnEditPrepareFunction(node, agentId){
-
-    appendTheCommonHTMLFile();
-
-    node.agentId = agentId;
-
-    await getThisAgentNodeActions(node);
-
-    updateParametersHtmlSection(node);
-
-    $("#node-input-agentId").on('change', async function() {
-        applyChangesForAgentChange(node);
-    }.bind(this));
-
-    $("#node-input-action").empty().append(`<option value=${node.action}>${node.action}</option>`);
-    node.actionsList.forEach(action => {
-        $("#node-input-action").append(`<option value="${action}">${action}</option>`);
-    });
-
-    $("#node-input-action").on('change', function(){
-        applyChangesForActionChange(node);
-    }.bind(this)); 
-
-    handleInvokeAction(node);
-}
--- nodes/room-booking-agent-node/RoomBookingAgent.js
+++ nodes/room-booking-agent-node/RoomBookingAgent.js
@@ -2,8 +2,22 @@ const imports = require('../../nodes/resources/imports.js');
 const js_common_methods = imports.js_common_methods_import;
 
 module.exports = function(RED){
+
+    var currentAction = null;
+
+    RED.httpAdmin.post('/currentAction', function(req, res) {
+        currentAction = req.body;
+    });
+
     function RoomBookingAgentNode(config){
-        js_common_methods.makeNodeConfiguration(RED, this, config);
+        RED.nodes.createNode(this,config);  
+        var node = this;
+        node.on('input', async function(msg){   
+            if(currentAction != null){
+                await js_common_methods.invokeAction(currentAction.actionName, currentAction.queryString, msg,node);
+                node.send(msg);
+            }
+        });
     }
     RED.nodes.registerType("RoomBookingAgent",RoomBookingAgentNode);
 }
